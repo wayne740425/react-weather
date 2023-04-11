e3da19f20d91337cedfa51d3f86f3ff884c2a855
Wayne <wayne@okmaster.com>
2023-04-11 18:36:50 +0800

    update pwa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'normalize.css';
-import App from './App';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import App from "./App";
+import "./styles.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
